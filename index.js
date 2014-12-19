9aca4eceb0e31d7af9d655d1cfcd2799d39e31db
CodeSergeant <lmrabenhorst@gmail.com>
2014-12-19 13:45:22 +1000

    Upload new files

---
+++ index.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//var fs = require('fs');
+
+app.get('/', function(req, res){
+    //var html = fs.readFileSync("index3.html", "utf8");
+    //res.send(html);
+    res.sendFile(__dirname + '/index.html');
+});
+console.log('connection achieved')
+
+
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	//console.log('hello connection');
+	socket.on("disconnect", function() {
+		console.log("user disconnected");
+	});
+	socket.on('user message', function(msg){
+		//console.log('hi all');
+		console.log('usermessage: ' + msg);
+		io.emit('user message', msg);
+	});
+	var n = 20;
+	while (n > 0) {
+		socket.emit('emic message', 'This is emic');
+		console.log('emic message: ' + 'This is emic');
+		n -= 1;
+	}
+	//socket.on('disconnect', function(){
+	//	console.log('user disconnected');
+	//});
+});
+
+http.listen(3001, function(){
+    console.log('listening on *:3001');
+});
