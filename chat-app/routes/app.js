16e77f25fa00fb0f05aa509fe85146d5a1cbed31
CodeSergeant <lmrabenhorst@gmail.com>
2015-01-09 15:30:11 +1000

    update

--- chat-app/routes/app.js
+++ chat-app/routes/app.js
@@ -21,39 +21,20 @@ app.get('/', function(req, res){
     //res.send(html);
     res.sendFile(__dirname + '/main.html');
 });
-ASQ()
-/*.then(function () {app = require('express')();
-report(app)})
-.then(report(app))
-.then(function () {http = require('http').Server(app)})
-.then(function () {io = require('socket.io')(http)})
-.then(function () {emic = require('./emic2.js')})
-.then(function () {report("Modules loaded successfully")})
-//.or(repErr("Module load error"))
-.then(report(emic))*/
-.then(emic.init())
-/*.then(
-	app.get('/', function(req, res){
-    //var html = fs.readFileSync("index3.html", "utf8");
-    //res.send(html);
-    	res.sendFile(__dirname + '/main.html');
-	})
-)*/
-.then(report('connection achieved'))
-.gate(
-	io.on('connection', function(socket){
-		report('a user connected');
-		socket.on('disconnect', function() {
-			report('a user disconnected');
-		});
-		socket.on('user message', function(msg){
-			report('sendmessage');
-			EMIC.speak(msg);
-			report('usermessage: ' + msg);
-			io.emit('user message', msg);
-		});
-	}),
-	http.listen(3001, function(){
-    	report('listening on *:3001');
-	})
-)
+io.on('connection', function(socket){
+	report('a user connected');
+	socket.on('disconnect', function() {
+		report('a user disconnected');
+	});
+	socket.on('user message', function(msg){
+		report('sendmessage');
+		EMIC.speak(msg);
+		report('usermessage: ' + msg);
+		io.emit('user message', msg);
+	});
+});
+http.listen(3001, function(){
+    report('listening on *:3001');
+});
+
+
