16b82b4e410c419bf9fbc316570afe3390f6748c
CodeSergeant <lmrabenhorst@gmail.com>
2015-02-16 18:00:16 +1000

    Close off day commit

---
+++ SpeakThis_GUI/sign-up-login-form (1)/submitMQTT.js
@@ -0,0 +1,15 @@
+
+var client = mqtt("mqtt://winter.ceit.uq.edu.au");
+
+function submitMQTT () {
+	var messageObject = {}
+	messageObject.priority = document.getElementById("priority").value || null;
+	messageObject.algorithm = document.getElementById("algorithm").value || null;
+	messageObject.speaker = document.getElementById("speaker").value || null;
+	messageObject.language = document.getElementById("language").value || null;
+	messageObject.volume = document.getElementById("volume").value || null;
+	messageObject.rate = document.getElementById("rate").value || null;
+	messageObject.text = document.getElementById("message").value || null;
+
+	client.publish("SpeakThis", JSON.stringify(messageObject));
+};
